6f65c3c2bc35f701b7c9bc0438eb2f7708052b20
AmanJain302 <amanjain0006@gmail.com>
2025-05-13 19:41:19 +0530

    converted to jsx

--- Lensroom/src/App.jsx
+++ Lensroom/src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import Profile3 from "./Pages/Profile3";
 import Booking from "./Pages/Booking";
+import Profile3 from "./Pages/Profile3";
 
 function App() {
   return (
     <Routes>
-      <Route path="/profile3" element={<Profile3 />} />
+      <Route path="/profile3" element={<Profile3/>} />
       <Route path="/booking" element={<Booking/>} />
     </Routes>
   );

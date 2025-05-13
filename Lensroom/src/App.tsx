e3deaf5b732ba2f5b171e3b82dda2af4db356a74
AmanJain302 <amanjain0006@gmail.com>
2025-05-13 18:56:55 +0530

    Created Booking Page

--- Lensroom/src/App.tsx
+++ Lensroom/src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Profile3 from "./Pages/Profile3";
+import Booking from "./Pages/Booking";
 
 function App() {
   return (
     <Routes>
       <Route path="/profile3" element={<Profile3 />} />
+      <Route path="/booking" element={<Booking/>} />
     </Routes>
   );
 }
